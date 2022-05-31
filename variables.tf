8b4b7e07a81f2008a12391a8060a7a0e368866ec
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-05-31 14:01:54 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -48,7 +48,6 @@ variable "org_description" {
 # vCloud Director Organization VDC Variables
 variable "vdc_alloc_model" {
     type        = string
-    default     = "AllocationVApp"
     description = "VDC Allocation Model"
 }
 variable "vdc_net_pool" {
