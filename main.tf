bdd211c3f28715e0eb04c3623a973aec3b5cb0a7
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-06-10 12:36:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,24 +94,3 @@ resource "vcd_org_vdc" "vdc-name" {
    depends_on = [vcd_network_direct.netdirect]
    
  }
-
-
- resource "vcd_vapp_vm" "firstVM" {
-  vapp_name     = var.vapp_name
-  name          = var.vapp_vm_name
-  computer_name = var.vapp_computer_name
-  catalog_name  = "App"
-  template_name = "Win2019Std_Splalic_NonMngmt_100322"
-  memory        = 2048
-  cpus          = 1
-
-network {
-      name = var.extnet_name
-      type = "org"
-      ip = "81.8.0.71"
-      ip_allocation_mode = "MANUAL"
-      is_primary = true
-  }
-
-
-}
