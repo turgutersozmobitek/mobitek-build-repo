fe2b5f6e6452780e9d9eead17492d5c377541fbb
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-06-10 12:01:09 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,6 @@ provider "vcd" {
   user                 = var.vcd_user
   password             = var.vcd_pass
   org                  = "System"
-  vdc                  = var.vdc_name
   url                  = var.vcd_url
   max_retry_timeout    = var.vcd_max_retry_timeout
   allow_unverified_ssl = var.vcd_allow_unverified_ssl
