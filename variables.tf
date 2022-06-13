a3849c117c786bd0ca9d1737f30e85b5a1008a5c
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-06-13 14:37:02 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -132,16 +132,6 @@ variable "vapp_description" {
     description = "vApp Açıklamasını giriniz."
 }
 
-variable "vapp_vm_name" {
-    type        = string
-    description = "vM adını giriniz.."
-}
-variable "vapp_computer_name" {
-    type        = string
-    description = "vM Computer Name Giriniz.."
-}
-
-
 variable "extnet_vcenter" {
     type        = string
     default     = "vCenter"
