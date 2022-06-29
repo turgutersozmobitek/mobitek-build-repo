1f6e87faa8005bbbf8e95511cab76d9452e3a9f9
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-06-29 09:16:26 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,10 +8,10 @@ terraform {
 }
 # Connect VMware vCloud Director Provider
 provider "vcd" {
-  user                 = "administrator"
-  password             =  "Mob12345!"
+  user                 = "timucin.devirmis"
+  password             =  "X9stFZsQpC4w!a"
   org                  = "System"
-  url                  = "https://81.8.0.53/api"
+  url                  = "https://31.145.174.203/api"
   max_retry_timeout    = "60"
   allow_unverified_ssl = "true"
 }
